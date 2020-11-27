2582e97b7dc3849b8806019f7f8e21c1e2387b47
ADMIN-KKZLUXIVL\Administrator <wrddshjh@163.com>
2020-11-27 21:41:37 +0800

    swiper

---
+++ traval/src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/202011/bdf238d87ac3aec799a78afa83662172.jpg_890x330_0a85c55b.jpg'
+      },
+        {
+          id: '0002',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/202011/bdf238d87ac3aec799a78afa83662172.jpg_890x330_0a85c55b.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+  .wrapper >>> .swiper-pagination-bullet-active
+    background:#fff
+  .wrapper
+    width: 100%
+    height: 0
+    overflow:hidden
+    padding-bottom:37%
+    background:#eee
+
+    .swiper-img
+      width:100%
+</style>
