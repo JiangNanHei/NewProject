b7ff9b602f6da7f9b6359f61b9b40edd8bc522a6
ADMIN-KKZLUXIVL\Administrator <wrddshjh@163.com>
2020-11-29 19:10:43 +0800

    路由

--- traval/src/pages/home/components/Header.vue
+++ traval/src/pages/home/components/Header.vue
@@ -5,10 +5,12 @@
       <span class="iconfont">&#xe7b3;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe62a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe62a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -49,6 +51,7 @@ export default {
       width: 1.24rem
       text-align center
       float:right
+      color:#fff
       .arrow-icon{
         font-size:.2rem
       }
